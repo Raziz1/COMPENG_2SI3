9a19e3fab705865d17e4b715252cee0ecc986368
Raziz1 <73625971+Raziz1@users.noreply.github.com>
2023-03-08 02:57:18 -0500

    Add files via upload

---
+++ Lab 3 - Implementation of Polynomial Using Linked List/PolyNode.h
@@ -0,0 +1,19 @@
+#ifndef POLYNODE_H_
+#define POLYNODE_H_
+
+class PolyNode
+{
+public:
+	int deg;
+	double coeff;
+	PolyNode* next;
+
+	// Polynomial Linked List Node
+	PolyNode(int d, double c, PolyNode* n) {
+		deg = d;
+		coeff = c;
+		next = n;
+	}
+};
+
+#endif /* POLYNODE_H_ */
